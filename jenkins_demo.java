b0964586e01f4adc9bde0508eff239d8aca030f4
anuragpatidar17 <anurag.patidar@yahoo.in>
2021-01-13 22:23:29 +0530

    first

---
+++ jenkins_demo.java
@@ -0,0 +1,32 @@
+package demo_package;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+public class jenkins_demo {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\TANMAY.AHUJA-b\\Desktop\\MDSTestData\\chromedriver.exe");
+	//	WebDriver driver = new WebDriver();
+		WebDriver driver = new ChromeDriver();
+		
+		 String baseUrl = "https://google.com";
+	       String expectedTitle = "Google";
+	       String actualTitle = "";
+	       driver.get(baseUrl);
+	      actualTitle = driver.getTitle();
+	      System.out.println(actualTitle);
+	      
+	       if (actualTitle.contentEquals(expectedTitle)){
+	            System.out.println("TEST PASSED!");
+	       } else {
+	           System.out.println("TEST FAILED");
+	       }
+	       driver.close();
+		
+	}
+//"
+}
